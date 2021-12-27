ce37f582cd06982d681287e8dc37062c98a16c9f
Chitsanupong <first30952@gmail.com>
2021-12-27 21:04:18 +0700

    feat: create stateless widget

--- sweet_order_app/lib/main.dart
+++ sweet_order_app/lib/main.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text(
-        'Sweet Order App',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'Kanit',
-          color: Colors.white
-        ),
-      ),
-      centerTitle: true,
-      backgroundColor: Colors.pink[300],
-      
-    ),
-    body: const Center(
-      child: Text("Let's order some sweet!"),
-    ),
-    floatingActionButton: FloatingActionButton
-      (
-      child: const Icon(Icons.add),
-      onPressed: () {},
-      backgroundColor: Colors.pink[300],
-    ),
-  ),
+void main() => runApp(const MaterialApp(
+  home: Home()
 ));
+
+class Home extends StatelessWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Sweet Order App',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Kanit',
+            color: Colors.white
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.pink[300],
+        
+      ),
+      body: const Center(
+        child: Text("Let's order some sweet!"),
+      ),
+      floatingActionButton: FloatingActionButton
+        (
+        child: const Icon(Icons.add),
+        onPressed: () {},
+        backgroundColor: Colors.pink[300],
+      ),
+    );
+  }
+}
